fc2c1a0b74309ea0c4ab72fa73d4b67acd6b2597
Pashok94 <pavel.kuzmin.1994@mail.ru>
2020-11-09 13:23:24 +0300

    hw2

--- src/java2_hw/hw2/MyArrayDataException.java
+++ src/java2_hw/hw2/MyArrayDataException.java
@@ -1,9 +1,6 @@
 package java2_hw.hw2;
 
 public class MyArrayDataException extends ClassCastException{
-    int iCoordinate;
-    int jCoordinate;
-
     public MyArrayDataException(int iCoordinate, int jCoordinate) {
         super("Некорректное значение в ячейке: " + iCoordinate + " - " + jCoordinate);
     }
