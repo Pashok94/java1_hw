16b6c7ea632d9efd59371357f6b3d230c2126c25
Pashok94 <pavel.kuzmin.1994@mail.ru>
2020-11-21 15:05:23 +0300

    hw5

---
+++ src/java2_hw/hw5/Main.java
@@ -0,0 +1,59 @@
+package java2_hw.hw5;
+
+import java.util.Arrays;
+
+public class Main {
+    static final int size = 10000000;
+    public static void main(String[] args) {
+        methodWithOneThread(size);
+        methodWithTwoThread(size);
+    }
+    public static void methodWithOneThread(int arrLength){
+        float[] arr = new float[arrLength];
+        Arrays.fill(arr, 1f);
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+    public static void methodWithTwoThread(int arrLength){
+        int h = arrLength / 2;
+        float[] arr = new float[arrLength];
+        float[] a1 = new float[h];
+        float[] a2  = new float[h];
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+}
